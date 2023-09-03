0d1d80c948d0a5bedfa271829cd9017a97f6b02d
Sabra <100771060+Sabraaa@users.noreply.github.com>
2023-09-03 20:25:52 +0330

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,60 @@
-import React from 'react';
+import React, { useState } from "react";
+import "./Navbar.css";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
+import images from "../../constants/images";
 
-import './Navbar.css';
-
-const Navbar = () => (
-  <div>
-    Navbar
-  </div>
+const Menu = () => (
+  <>
+    <li className="p__opensans">
+      <a href="#home"> Home</a>
+    </li>
+    <li className="p__opensans">
+      <a href="#about"> About</a>
+    </li>
+    <li className="p__opensans">
+      <a href="#menu"> Menu</a>
+    </li>
+    <li className="p__opensans">
+      <a href="#awards"> Awards</a>
+    </li>
+    <li className="p__opensans">
+      <a href="#contact"> Contact</a>
+    </li>
+  </>
 );
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  return (
+    <div className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.gericht} alt="apps logo" />
+      </div>
+      <ul className="app__navbar-links">
+        <Menu />
+      </ul>
+      <div className="app__navbar-login">
+        <a href="#login" className="p__opensans">
+          Log in / Register
+        </a>
+        <div />
+        <a href="/" className="p__opensans">
+          Book Table
+        </a>
+      </div>
+      <div className="app__navbar-smallscreen">
+        <GiHamburgerMenu onClick={() => setToggleMenu(false)} />
+        {toggleMenu && (
+        <div className="app__navbar-smallscreen_overlay">
+          <MdOutlineRestaurantMenu />
+          <ul className="app__navbar-smallsccreen-links">
+            <Menu />
+          </ul>
+        </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
